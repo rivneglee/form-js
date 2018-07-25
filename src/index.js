d0527395d8126914c4d01277097147a2c6fde38c
Cheng <cheng.li@rea-group.com>
2018-07-25 10:16:01 +0800

    Add Map, isArray polyfills

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import './polyfills';
 import 'font-awesome/css/font-awesome.css';
+import Workspace from './components/workspace';
+import Canvas from './components/flexible-canvas';
 
-export { default as Workspace } from './components/workspace';
-export { default as Canvas } from './components/flexible-canvas';
+export {
+  Workspace,
+  Canvas,
+};
--- src/polyfills/index.js
+++ src/polyfills/index.js
@@ -1,3 +1,8 @@
 import 'es6-string-polyfills';
 import 'polyfill-array';
 import 'babel-polyfill';
+import 'core-js/fn/object/assign';
+import 'core-js/fn/array/from';
+import 'core-js/fn/array/is-array';
+import 'core-js/fn/map';
+import 'core-js/fn/set';
